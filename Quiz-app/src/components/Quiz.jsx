6534c042c352f442c2248ad2346e171e240c777f
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-06 15:30:03 +0200

    quiz changes

--- Quiz-app/src/components/Quiz.jsx
+++ Quiz-app/src/components/Quiz.jsx
@@ -1,29 +1,57 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
-import QUESTIONS from '../questions.js'
+import QUESTIONS from "../questions.js";
+import quizIsCompleteImg from '../assets/quiz-complete.png'
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  const  activeQuestionIndex= userAnswers.length;
+  const activeQuestionIndex = userAnswers.length;
 
-  function handleSelectAnswer(selectedAnswer) {
-    setUserAnswers(prevAnswers => {
-        return [...prevAnswers,selectedAnswer]
-    })
+
+  
+  function handleSelectAnswer (selectedAnswer)  {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer];
+    });
+    
   }
 
-  return(
-    <div id="question">
+  if(activeQuestionIndex == QUESTIONS.length){
+    return(
+      <div id="summary">
+                <img src={quizIsCompleteImg} alt="quizIsCompleteImg" />
+                <h2>Quiz Completed!</h2>
+            </div>
+          )
+        }
+        
+        const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+        shuffledAnswers.sort(() => Math.random() - 0.5);
+  // const handleSelectAnswer = useCallback((selectedAnswer) => {
+  //     setUserAnswers((prevAnswers) => {
+  //         return [...prevAnswers, selectedAnswer];
+  //       });
+  //   },[n])
+
+  
+  // const handlesSkipQuestion = useCallback(() => handleSelectAnswer(null),[handleSelectAnswer])
+    
+  return (
+    <div id="quiz">
+      <div id="question">
+      <QuestionTimer timeout={5000} onTimeout={ () =>  handleSelectAnswer(null)}/>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-            {QUESTIONS[activeQuestionIndex].answers.map(x => {
-                return(
-                <li key={x} className="answer">
-                    <button>{x}</button>
-                </li>
-                )
-            })}
+          {shuffledAnswers.map((x) => {
+            return (
+              <li key={x} className="answer">
+                <button onClick={() => handleSelectAnswer(x)}>{x}</button>
+              </li>
+            );
+          })}
         </ul>
+      </div>
     </div>
-     )
-  }
+  );
+}
