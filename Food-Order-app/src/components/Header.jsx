c8c5eed1ad49131dc1472518da4889281eb957d3
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-10 20:09:35 +0200

    button component

--- Food-Order-app/src/components/Header.jsx
+++ Food-Order-app/src/components/Header.jsx
@@ -1,4 +1,6 @@
 import logo from '../assets/logo.jpg' 
+import Button from './UI/Button.jsx'
+
 
 export default function Header() {
     return <header id="main-header">
@@ -7,7 +9,7 @@ export default function Header() {
             <h1 >FOOD ORDER</h1>
         </div>
         <nav>
-            <button>Cart (0)</button>
+            <Button textOnly={true}>Cart (0)</Button>
         </nav>
     </header>
 }
--- Food-Order-app/src/components/MealItem.jsx
+++ Food-Order-app/src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import Button from "./UI/Button.jsx";
+
 export default function MealItem({meal}) {
   return (
     <li key={meal.id} className="meal-item">
@@ -5,11 +7,11 @@ export default function MealItem({meal}) {
         <img src={`http://localhost:3000/${meal.image}`} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">${meal.price}</p>
           <p>{meal.description}</p>
         </div>
         <p className="meal-item-actions">
-          <button>Add to Cart</button>
+          <Button>Add to Cart</Button>
         </p>
       </article>
     </li>
