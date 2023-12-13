7c38eb5bd66cfbb090d148537b7a688cf46c204a
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-13 18:11:34 +0200

    error handling

--- Food-Order-app/src/components/Meals.jsx
+++ Food-Order-app/src/components/Meals.jsx
@@ -1,14 +1,22 @@
 // import { useState } from "react"
 // import { useEffect } from "react"
 import useHttp from "../hooks/useHttp.js"
+import Error from "./Error.jsx"
 import MealItem from "./MealItem.jsx"
 
 
 export default function Meals() {
-    const {data : meals,loading} = useHttp('http://localhost:3000/meals',[])
+    const {data : meals,loading,errors} = useHttp('http://localhost:3000/meals',[])
+
+
+    if(errors){
+        return(
+            <Error title={'Failed to fetch meals'} message={errors}/>
+        )
+    }
 
     if(loading) {
-        return <p>Loading meals...</p>
+        return <p className="center">Loading meals...</p>
     }
 
     return (
